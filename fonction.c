f87c4d6c7edf27c849efef4d55277148a79d2406
TuxStory <ajm.even@gmail.com>
2020-12-17 18:02:17 +0100

    test visual

--- fonction.c
+++ fonction.c
@@ -10,7 +10,7 @@ int main(void)
 {
     int balleX;
     balleX = init_ball(balleX);
-    printf("Valeur de X de la Balle %d\n",balleX);
+    printf("Valeur de X de la Balle : %d\n",balleX);
 
     return 0;   
 }
