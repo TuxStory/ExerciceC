f00c39ede76ed552d7c1fb741074fea455e9d0bc
TuxStory <ajm.even@gmail.com>
2020-12-17 19:59:05 +0100

    init

--- fonction.c
+++ fonction.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-int init_ball(int posX)
-{
-    posX = 150;
-    return posX;
-}
+int init_ball(int posX);
 
 int main(void)
 {
     int balleX;
     balleX = init_ball(balleX);
-    printf("Valeur de X de la Balle : %d\n",balleX);
+    printf("Valeur de X de la Balle :%d\n",balleX);
 
     return 0;   
+}
+
+int init_ball(int posX)
+{
+    posX = 150;
+    return posX;
 }
