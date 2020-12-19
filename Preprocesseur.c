b6e0f1d04376228e2798f2ce7aa7572946156039
TuxStory <ajm.even@gmail.com>
2020-12-19 18:00:54 +0100

    Prépro3

--- Preprocesseur.c
+++ Preprocesseur.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
-#define TVA 25
+#define TVA 25 // Defini une valeur - remplace TVA par 25. 
+#define affiche printf // Permet de créer des mots clés.
+#define LONGUEUR 15
+#define LARGEUR 5
+#define AIRE (LONGUEUR * LARGEUR)
+
+/* Constantes Prédéfinies di language C : 
+
+    _FILE_ nom du fichier
+    _LINE_ ligne du fichier
+    _TIME_ heure de la compilation
+*/
+
 int main(void)
 {
     int prixHT = 150;
@@ -10,5 +22,7 @@ int main(void)
     printf("La TVA est de %d.\n",TVA);
     printf("Le prix de l'objet est de %d.\n",prixTTC);
 
+    affiche("Hello World!\n");
+    affiche("Aire du rectangle %d.\n",AIRE);
     return 0;
 }
